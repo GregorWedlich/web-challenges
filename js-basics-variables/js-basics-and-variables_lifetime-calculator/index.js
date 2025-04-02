3e89d7a5256f994c1c050f9199098a4f8d2be28f
Gregor Wedlich <hello@sil3ntrunning.net>
2025-04-02 11:34:04 +0200

    Edit: Task Lifetime Calculator

--- js-basics-variables/js-basics-and-variables_lifetime-calculator/index.js
+++ js-basics-variables/js-basics-and-variables_lifetime-calculator/index.js
@@ -18,5 +18,26 @@ Hint: You can combine text and variables using the `+` operator.
 */
 
 const currentAge = Number(process.argv[2]); // This takes the `<age>` entered in the command `node index.js <age>`.
+const daysInYear = 365;
+const averageLifeSpan = 80;
+const hoursInDay = 24;
+const sleepHoursPerDay = 8;
 
-console.log("Your current age is: " + currentAge);
+// 1
+const totalDaysLived = currentAge * daysInYear;
+
+// 2
+const estimatedRemainingDays = (averageLifeSpan - currentAge) * daysInYear;
+
+// 3
+const percentageOfLifeLived = (currentAge / averageLifeSpan) * 100;
+
+// 4
+const sleepDays = (sleepHoursPerDay / hoursInDay) * daysInYear * currentAge;
+
+console.log("Total days lived: " + totalDaysLived);
+console.log("Estimated remaining days: " + estimatedRemainingDays);
+console.log("Percentage of life lived: " + percentageOfLifeLived + "%");
+console.log("Total days spent sleeping: " + sleepDays);
+
+console.log("Current age is: " + currentAge);
