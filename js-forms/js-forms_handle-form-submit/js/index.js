579d8bc17da885bf3d793e3de18039fe4770020d
Gregor Wedlich <hello@sil3ntrunning.net>
2025-04-08 11:20:51 +0200

    Task: Part 1: Handle Form Submit

---
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -0,0 +1,19 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  // Bonus
+  console.log(
+    `The age-badness-sum of ${data.firstName} is ${
+      Number(data.age) + Number(data.badness)
+    }`
+  );
+});
