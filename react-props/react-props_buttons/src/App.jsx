74c51fd1359104e8ba51ae09b75095fd34bb791f
Gregor Wedlich <hello@sil3ntrunning.net>
2025-04-22 15:47:57 +0200

    Add: Task Button Pass a Function as a Prop

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,17 +1,24 @@
 export default function App() {
-  return <Button color={"red"} disabled={false} text={"Mein Button"} />;
-}
-
-function Button({ color, disabled, text }) {
   function handleClick() {
     alert("You clicked me!");
   }
 
   return (
+    <Button
+      color={"red"}
+      disabled={false}
+      text={"Mein Button"}
+      onEventClick={handleClick}
+    />
+  );
+}
+
+function Button({ color, disabled, text, onEventClick }) {
+  return (
     <button
       style={{ backgroundColor: color }}
       disabled={disabled}
-      onClick={handleClick}
+      onClick={onEventClick}
     >
       {text}
     </button>
