e76a271e9817f53876cbf810d54db48549e29fcc
Gregor Wedlich <hello@sil3ntrunning.net>
2025-04-26 00:35:03 +0200

    Add: Task React State 2 table-reservation

--- react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,27 @@
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  // console.log(people);
+
+  function handleIncrement() {
+    setPeople((prevPeople) => prevPeople + 1);
+  }
+  function handleDecrement() {
+    setPeople((prevPeople) => (prevPeople > 0 ? prevPeople - 1 : 0));
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        onHandleIncrement={handleIncrement}
+        onHandleDecrement={handleDecrement}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
