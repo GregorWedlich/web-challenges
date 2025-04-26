99c689b4c364f1591453c21befdbd3e21756c5bb
Gregor Wedlich <hello@sil3ntrunning.net>
2025-04-26 13:04:38 +0200

    Add: Task React State 2 personal details

--- react-state-2/react-state-2_personal-details/src/components/Form.jsx
+++ react-state-2/react-state-2_personal-details/src/components/Form.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-
-export default function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
+export default function Form({ onSubmit }) {
   function handleSubmit(event) {
     event.preventDefault();
+
+    onSubmit(event);
   }
 
   return (
