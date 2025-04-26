f36fd352cad451a66cfceea8ae2e31ada781540c
Gregor Wedlich <hello@sil3ntrunning.net>
2025-04-26 14:52:33 +0200

    Add: Task React State 3 adding animals

---
+++ react-state-3/react-state-3_adding-animals/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { uid } from "uid";
+import "./styles.css";
+import Form from "./components/Form";
+import List from "./components/List";
+
+const initialAnimals = [
+  {
+    id: "mTBErig",
+    name: "Octopus",
+    emoji: "🐙",
+  },
+  {
+    id: "iMsbXQ1",
+    name: "Crocodile",
+    emoji: "🐊",
+  },
+  {
+    id: "Qjf_K7V",
+    name: "Beaver",
+    emoji: "🦫",
+  },
+];
+
+export default function App() {
+  const [animals, setAnimals] = useState(initialAnimals);
+
+  function handleAddAnimal(newAnimal) {
+    setAnimals((prevAnimals) => {
+      return [...prevAnimals, { ...newAnimal, id: uid() }];
+    });
+  }
+
+  return (
+    <main className="app">
+      <Form onAddAnimal={handleAddAnimal} />
+      <List animals={animals} />
+    </main>
+  );
+}
